070e742bd36aa066fc0f3a648ed4bededbd556be
Dawid Chec <dawid.chec@testscouts.se>
2024-09-03 15:08:37 +0200

    Adding string_functions.c

---
+++ string_functions.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+
+char string1[] = "Dawid";
+char string2[] = "Chec";
+
+//strlwr(string1);                    // converts a string to lowercase
+//strupr(string1);                    // converts a string to uppercase
+//strcat(string1, string2);           // appends string2 to end of string1
+//strncat(string1, string2, 1);       // appends n characters from string2 to string1
+//strcpy(string1, string2)            // copy string2 to string1  
+//strncpy(string1, string2);          // copy characters of string2 to string1
+
+//strset(string1, '?');               // sets all charaters of a string to a given character
+//strnset(string1, 'x', 1);           // sets frist n character of a string to a given character
+//strrev(string1);                    // reverses a string
+
+
+
+
+printf("%s", string1);      
+
+
+
+return 0;
+}
