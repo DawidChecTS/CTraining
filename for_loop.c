ba3c2104102ff30a7e76e802575f1653a59e44fc
Dawid Chec <dawid.chec@testscouts.se>
2024-09-04 10:39:26 +0200

    Adding for_loop.c file

---
+++ for_loop.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+
+    for(int i = 1; i <= 10; i++) // for loop that counts from 1 to 1000 until the number is 10.
+    {
+        printf("%d\n", i);
+    }
+    for(int number = 1996; number <= 2024; number++)
+    {
+        printf("%d One year is gone.\n", number);
+    }
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d counting...\n", i);
+        if (i == 10){
+            	printf("There is a ten\n");
+        }
+        else
+        {
+            printf("Not ten yet!\n");
+        }
+        
+    }
+    for (double i = 1.001; i <= 2.000; i += 0.003) 
+    {
+        printf("%.3f\n", i);
+    }
+
+return 0;
+}
