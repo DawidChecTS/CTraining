9746a9ef6f560a3a4355862d19a39e9e6580754f
Dawid Chec <dawid.chec@testscouts.se>
2024-09-05 16:43:55 +0200

    Adding unfinished 2d_arrays file

---
+++ 2d_arrays.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+
+    // 2D array = an array, where each element is an entire array
+    //            usefull if you need a matrix, grid, or table of data
+
+    /*
+    int numbers[3][3] = {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+    };
+    */
+
+    int numbers[3][3]; // how to initialize an array
+
+
+        numbers[0][0] = 1; 
+        numbers[0][1] = 2;
+        numbers[0][2] = 3; 
+        numbers[1][0] = 4; 
+        numbers[1][1] = 5; 
+        numbers[1][2] = 6; 
+        numbers[2][0] = 7; 
+        numbers[2][1] = 8; 
+        numbers[2][2] = 9; 
+
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++){
+                printf("%d ", numbers[i][j]);
+            }
+            printf("\n");
+        }
+
+
+    return 0;
+}
