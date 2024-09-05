6b003ee00dabe37be0b96879cf5cd8729cc13ad2
Dawid Chec <dawid.chec@testscouts.se>
2024-09-05 10:51:38 +0200

    Adding nested_loop.c file

---
+++ nested_loop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+    int columns;
+    char symbols;
+
+    printf("Type in number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Type in number of columns: ");
+    scanf("%d", &columns);
+
+    scanf("%c");
+
+    printf("Choose your character to be displayed: ");
+    scanf("%c", &symbols);
+
+    for(int i = 1; i <= rows; i ++)
+    {
+        for (int j = 1; j <= columns; j++)
+        {
+            printf("%c", symbols);
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
