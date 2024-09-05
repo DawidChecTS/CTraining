c45fb2d628b74aedae80c288c877b6a7710a2b6c
Dawid Chec <dawid.chec@testscouts.se>
2024-09-05 17:11:13 +0200

    Finishing 2d_arrays.c file

--- 2d_arrays.c
+++ 2d_arrays.c
@@ -15,25 +15,28 @@ int main() {
 
     int numbers[3][3]; // how to initialize an array
 
+    int rows = sizeof(numbers)/sizeof(numbers[0]); // how to calculate number of rows
+    int columns = sizeof(numbers[0])/sizeof(numbers[0][0]); // how to calculate number of columns
 
-        numbers[0][0] = 1; 
-        numbers[0][1] = 2;
-        numbers[0][2] = 3; 
-        numbers[1][0] = 4; 
-        numbers[1][1] = 5; 
-        numbers[1][2] = 6; 
-        numbers[2][0] = 7; 
-        numbers[2][1] = 8; 
-        numbers[2][2] = 9; 
+    printf("rows: %d\n", rows);
+    printf("columns: %d\n", columns);
 
-        for(int i = 0; i < 3; i++)
-        {
-            for(int j = 0; j < 3; j++){
-                printf("%d ", numbers[i][j]);
-            }
-            printf("\n");
+    numbers[0][0] = 1; 
+    numbers[0][1] = 2;
+    numbers[0][2] = 3; 
+    numbers[1][0] = 4; 
+    numbers[1][1] = 5; 
+    numbers[1][2] = 6; 
+    numbers[2][0] = 7; 
+    numbers[2][1] = 8; 
+    numbers[2][2] = 9; 
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < columns; j++){
+            printf("%d ", numbers[i][j]);
         }
-
-
+        printf("\n");
+    }
+        
     return 0;
 }
