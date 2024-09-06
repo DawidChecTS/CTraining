02426b61ad09b8fdedf1b838ec6a78ec604a5206
Dawid Chec <dawid.chec@testscouts.se>
2024-09-06 09:03:29 +0200

    Adding 2d arrays.c file

--- 2d_arrays.c
+++ 2d_arrays.c
@@ -13,7 +13,7 @@ int main() {
     };
     */
 
-    int numbers[3][3]; // how to initialize an array
+    int numbers[4][4]; // how to initialize an array
 
     int rows = sizeof(numbers)/sizeof(numbers[0]); // how to calculate number of rows
     int columns = sizeof(numbers[0])/sizeof(numbers[0][0]); // how to calculate number of columns
@@ -21,15 +21,22 @@ int main() {
     printf("rows: %d\n", rows);
     printf("columns: %d\n", columns);
 
-    numbers[0][0] = 1; 
+    numbers[0][0] = 1;
     numbers[0][1] = 2;
     numbers[0][2] = 3; 
-    numbers[1][0] = 4; 
-    numbers[1][1] = 5; 
-    numbers[1][2] = 6; 
-    numbers[2][0] = 7; 
-    numbers[2][1] = 8; 
-    numbers[2][2] = 9; 
+    numbers[0][3] = 4;
+    numbers[1][0] = 5; 
+    numbers[1][1] = 6; 
+    numbers[1][2] = 7; 
+    numbers[1][3] = 8;
+    numbers[2][0] = 9; 
+    numbers[2][1] = 10; 
+    numbers[2][2] = 11; 
+    numbers[2][3] = 12;
+    numbers[3][0] = 13; 
+    numbers[3][1] = 14; 
+    numbers[3][2] = 15; 
+    numbers[3][3] = 16;
 
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < columns; j++){
